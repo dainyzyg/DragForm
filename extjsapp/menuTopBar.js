0edc2c3689a561a3ac5d8f6ae4963dbc0298eed6
dainyzyg <470067721@qq.com>
2016-01-21 14:50:57 +0800

    添加表单代码导出功能

--- extjsapp/menuTopBar.js
+++ extjsapp/menuTopBar.js
@@ -4,18 +4,40 @@
 Ext.define("extjsapp.menuTopBar", {
     xtype: 'extjsapp.menuTopBar',
     exportForm(){
+        showFormJson = this.showFormJson
+        showFormJson.dropFormID = this.dropFormID
         var fieldMap = Ext.getCmp(this.dropFormID).extraData.fieldMap
         var code = JSON.stringify([...fieldMap])
         Ext.tip.QuickTipManager.init()
         Ext.create('Ext.window.Window', {
             title: 'FormItems',
+            //autoScroll: true,
             height: 600,
             width: 800,
             layout: 'fit',
-            html: '<iframe scrolling="no" frameborder="0" src="codemirrortest.html" width=100% height=100%/>'
+            html: '<iframe onload="showFormJson(this)" frameborder="0" src="FormCodeShow.html" width=100% height=100%/>'
         }).show()
     },
-    constructor: function (cf) {
+    showFormJson(e){
+        var form = Ext.getCmp(showFormJson.dropFormID)
+        var fieldMap = form.extraData.fieldMap
+        var formJson = []
+        for (var item of form.items.items) {
+            formJson.push(fieldMap.get(item.id))
+        }
+        console.log(formJson)
+        var codetext = JSON.stringify(formJson, null, '\t')
+        var CodeMirror = e.contentWindow.CodeMirror
+        console.log(CodeMirror)
+        console.log(e.contentWindow.document.body)
+        var myCodeMirror = CodeMirror(e.contentWindow.document.body, {
+            value: codetext,
+            lineNumbers: true,
+            readOnly: true,
+            mode: "application/ld+json"
+        })
+    },
+    constructor (cf){
         var that = this
         Object.assign(this, cf)
         that.items = [
